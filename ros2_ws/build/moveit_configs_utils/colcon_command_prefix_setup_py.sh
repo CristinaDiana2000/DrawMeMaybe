a52a69481a89abd17924ce195fe55f6910857af2
cristina <cristina.tutunariu@tha.de>
2025-12-18 12:14:46 +0100

    bild von ros_ws geändert

---
+++ ros2_ws/build/moveit_configs_utils/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/drawmemaybe/DrawMeMaybe/ros2_ws/install/launch_param_builder/share/launch_param_builder/package.sh"
+. "/home/drawmemaybe/DrawMeMaybe/ros2_ws/install/srdfdom/share/srdfdom/package.sh"

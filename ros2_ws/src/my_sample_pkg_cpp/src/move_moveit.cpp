81a149c83f6d69aca44836eb1b98047d81fa54bb
cristina <cristina.tutunariu@yahoo.com>
2026-01-07 12:39:53 +0100

    Build 07.01.

--- ros2_ws/src/my_sample_pkg_cpp/src/move_moveit.cpp
+++ ros2_ws/src/my_sample_pkg_cpp/src/move_moveit.cpp
@@ -45,12 +45,13 @@ int main(int argc, char **argv){
 
 	//Go Home
 	RCLCPP_INFO(LOGGER, "Going Home");
-	joint_group_positions_arm[0] = 0.00; //Shoulder Pan
-	joint_group_positions_arm[1] = -2.50; //Shoulder Lift
-	joint_group_positions_arm[2] = 1.50; //Elbow
-	joint_group_positions_arm[3] = -1.50; //Wrist 1
-	joint_group_positions_arm[4] = -1.55; //Wrist 2
-	//joint_group_positions_arm[5] = 0.00; //Wrist 3
+	double deg2rad = M_PI / 180.0;
+	joint_group_positions_arm[0] = -76.25 * deg2rad; //Shoulder Pan
+	joint_group_positions_arm[1] = -81.76 * deg2rad; //Shoulder Lift
+	joint_group_positions_arm[2] = 110.62 * deg2rad; //Elbow
+	joint_group_positions_arm[3] = 238.40 * deg2rad; //Wrist 1
+	joint_group_positions_arm[4] = -92.08 * deg2rad; //Wrist 2
+	//joint_group_positions_arm[5] = 0.00* deg2rad; //Wrist 3
 	move_group_arm.setJointValueTarget(joint_group_positions_arm);
 	moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
 	bool success_arm = (move_group_arm.plan(my_plan_arm) == moveit::core::MoveItErrorCode::SUCCESS);
@@ -58,14 +59,36 @@ int main(int argc, char **argv){
 
 	//Pregrasp
 	RCLCPP_INFO(LOGGER, "Pregrasp Position");
+
+	moveit_msgs::msg::OrientationConstraint ori_constraint;
 	geometry_msgs::msg::Pose target_pose1;
-	target_pose1.orientation.x = -1.0;
-	target_pose1.orientation.y = 0.00;
-	target_pose1.orientation.z = 0.00;
-	target_pose1.orientation.w = 0.00;
-	target_pose1.position.x = 0.343;
-	target_pose1.position.y = 0.132;
-	target_pose1.position.z = 0.264;
+
+	target_pose1.orientation.x = 0.9996;
+	target_pose1.orientation.y = -0.0177;
+	target_pose1.orientation.z = -0.0020;
+	target_pose1.orientation.w = -0.0212;
+	target_pose1.position.x = 0.136;
+	target_pose1.position.y = -0.447;
+	target_pose1.position.z = 0.312;
+
+	ori_constraint.header.frame_id = move_group_arm.getPlanningFrame();
+	ori_constraint.link_name = move_group_arm.getEndEffectorLink();
+
+	// gewünschte Orientierung (z. B. aus target_pose1)
+	ori_constraint.orientation = target_pose1.orientation;
+
+	// Toleranzen (rad)
+	ori_constraint.absolute_x_axis_tolerance = 0.05;
+	ori_constraint.absolute_y_axis_tolerance = 0.05;
+	ori_constraint.absolute_z_axis_tolerance = 0.05;
+
+	ori_constraint.weight = 1.0;
+
+	moveit_msgs::msg::Constraints path_constraints;
+	path_constraints.orientation_constraints.push_back(ori_constraint);
+
+	move_group_arm.setPathConstraints(path_constraints);
+
 	move_group_arm.setPoseTarget(target_pose1);
 	success_arm = (move_group_arm.plan(my_plan_arm) ==
 	moveit::core::MoveItErrorCode::SUCCESS);
@@ -73,27 +96,43 @@ int main(int argc, char **argv){
 
 	//Approach
 	RCLCPP_INFO(LOGGER, "Approach to object!");
-	std::vector<geometry_msgs::msg::Pose>approach_waypoints;
-	target_pose1.position.z -= 0.03;
-	approach_waypoints.push_back(target_pose1);
-	target_pose1.position.z -= 0.03;
-	approach_waypoints.push_back(target_pose1);
-	moveit_msgs::msg::RobotTrajectory trajectory_approach;
-	const double jump_threshold = 0.0;
-	const double eef_step = 0.01;
-	double fraction = move_group_arm.computeCartesianPath(approach_waypoints, eef_step, jump_threshold, trajectory_approach);
-	move_group_arm. execute(trajectory_approach);
+    std::vector<geometry_msgs::msg::Pose> approach_waypoints;
+
+    geometry_msgs::msg::Pose start_pose = move_group_arm.getCurrentPose().pose;
+    geometry_msgs::msg::Pose p1 = start_pose;
+    p1.position.z -= 0.01;
+    approach_waypoints.push_back(p1);
+
+    geometry_msgs::msg::Pose p2 = p1;
+    p2.position.z -= 0.01;
+    approach_waypoints.push_back(p2);
+
+    moveit_msgs::msg::RobotTrajectory trajectory_approach;
+    const double jump_threshold = 0.0;
+    const double eef_step = 0.01;
+    double fraction = move_group_arm.computeCartesianPath(approach_waypoints, eef_step, jump_threshold, trajectory_approach);
+    move_group_arm.execute(trajectory_approach);
+
+    if (fraction < 0.99)
+        RCLCPP_WARN(LOGGER, "Nur %.1f %% des Pfades geplant!", fraction * 100.0);
 
 	//Retreat
-	RCLCPP_INFO(LOGGER,"Retreat from object!");
-	std::vector<geometry_msgs::msg::Pose>retreat_waypoints;
-	target_pose1.position.z += 0.03;
-	retreat_waypoints.push_back(target_pose1);
-	target_pose1.position.z += 0.03;
-	retreat_waypoints.push_back(target_pose1);
-	moveit_msgs::msg::RobotTrajectory trajectory_retreat;
-	fraction = move_group_arm.computeCartesianPath(retreat_waypoints, eef_step,
-	jump_threshold, trajectory_retreat);
+	RCLCPP_INFO(LOGGER, "Retreat from object!");
+    std::vector<geometry_msgs::msg::Pose> retreat_waypoints;
+
+    geometry_msgs::msg::Pose retreat_start = move_group_arm.getCurrentPose().pose;
+    geometry_msgs::msg::Pose r1 = retreat_start;
+    r1.position.y += 0.03;
+    retreat_waypoints.push_back(r1);
+
+    geometry_msgs::msg::Pose r2 = r1;
+    r2.position.y += 0.03;
+    retreat_waypoints.push_back(r2);
+
+    moveit_msgs::msg::RobotTrajectory trajectory_retreat;
+    fraction = move_group_arm.computeCartesianPath(retreat_waypoints, eef_step, jump_threshold, trajectory_retreat);
+    move_group_arm.execute(trajectory_retreat);
+
 	rclcpp::shutdown();
 	return 0;
 }

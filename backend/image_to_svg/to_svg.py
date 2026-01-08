dfa6555818e9d41ca69fdb8c1d4fb12b44c604c4
mhmtk <muhammet.keser@tha.de>
2026-01-08 17:00:12 +0100

    to_svg strukturierter

--- backend/image_to_svg/to_svg.py
+++ backend/image_to_svg/to_svg.py
@@ -1,5 +1,8 @@
 import cv2 as cv2
 import numpy as np
+from svgpathtools import svg2paths, Arc, Line, QuadraticBezier, CubicBezier
+import json
+
 
 def contours_to_svg(contours, filename="out.svg", width=None, height=None):
     # Breite/Höhe optional für viewBox
@@ -16,58 +19,72 @@ def contours_to_svg(contours, filename="out.svg", width=None, height=None):
         f.write('</svg>')
         
 
+def to_contours(img):
 
-img = cv2.imread('C:\\Users\\muham\\repos\\DrawMeMaybe\\src\\assets\\ChatGPT Image 16. Nov. 2025, 15_35_26.png', cv2.IMREAD_GRAYSCALE)
+    #pixel in liste umwandeln
+    pixels = img.reshape((-1, 1)).astype(np.float32)
 
-#pixel in liste umwandeln
-pixels = img.reshape((-1, 1)).astype(np.float32)
+    #Kmeans parameter
+    # Define criteria = ( type(beide ausgewählt), max_iter = 30 , epsilon = 0.2 (min änderung nach iteration) )
+    criteria = (cv2.TermCriteria_EPS + cv2.TermCriteria_MAX_ITER, 30, 0.2)
+    _k, labels, centers = cv2.kmeans(
+        pixels,           # Daten
+        3,                # Anzahl Cluster (hell, mittel, dunkel)
+        None,             # Keine Initiallabels
+        criteria,         # Kriterien
+        10,               # Anzahl der Initialisierungen (versuche)
+        cv2.KMEANS_RANDOM_CENTERS
+    )
+    #cluster zurück ins bild
+    segmented = centers[labels.flatten()].reshape(img.shape).astype("uint8")
 
-#Kmeans parameter
-# Define criteria = ( type(beide ausgewählt), max_iter = 30 , epsilon = 0.2 (min änderung nach iteration) )
-criteria = (cv2.TermCriteria_EPS + cv2.TermCriteria_MAX_ITER, 30, 0.2)
-_k, labels, centers = cv2.kmeans(
-    pixels,           # Daten
-    3,                # Anzahl Cluster (hell, mittel, dunkel)
-    None,             # Keine Initiallabels
-    criteria,         # Kriterien
-    10,               # Anzahl der Initialisierungen (versuche)
-    cv2.KMEANS_RANDOM_CENTERS
-)
-#cluster zurück ins bild
-segmented = centers[labels.flatten()].reshape(img.shape).astype("uint8")
-
-blurred = cv2.GaussianBlur(segmented, (5, 5), 0) #canny sonst empfindlich gegen Bildrauschen und krassen Kontrast
-edges = cv2.Canny(blurred, 80, 150)  #thresholds1 = vllt Kante, threshold2 = starke Kante
-
-
-#konturen finden
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
-top_n = 150
-top_contours = contours[:top_n]
-
-#richtung SVG
-approx_contours = [cv2.approxPolyDP(cnt, epsilon=2, closed=True) for cnt in top_contours]
-contours_to_svg(approx_contours, "top150.svg")
+    blurred = cv2.GaussianBlur(segmented, (5, 5), 0) #canny sonst empfindlich gegen Bildrauschen und krassen Kontrast
+    edges = cv2.Canny(blurred, 80, 150)  #thresholds1 = vllt Kante, threshold2 = starke Kante
 
 
-canvas = np.ones_like(edges) * 255  # weißer Hintergrund
-cv2.drawContours(canvas, top_contours, -1, (0,0,0), 1)  # schwarze Linien, dicke=1
+    #konturen finden
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    top_n = 150
+    top_contours = contours[:top_n]
 
-cv2.imshow("Top 100 Contours", cv2.resize(canvas,None, fx=0.8, fy=0.8))
+    #SVG
+    approx_contours = [cv2.approxPolyDP(cnt, epsilon=2, closed=True) for cnt in top_contours]
+    return approx_contours
+
+
+INTERP_STEP = 1.0  # 1 mm zwischen Punkten
 
 
 
-
-cv2.imshow("Original", cv2.resize(img,None, fx=0.4, fy=0.4))
-cv2.imshow("Segmented", cv2.resize(segmented,None, fx=0.4, fy=0.4))
-cv2.imshow("blurred", cv2.resize(blurred,None, fx=0.4, fy=0.4))
-cv2.imshow("edges", cv2.resize(edges,None, fx=0.8, fy=0.8))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def main(args=None):
+    img = cv2.imread('C:\\Users\\muham\\repos\\DrawMeMaybe\\src\\assets\\ChatGPT Image 16. Nov. 2025, 15_35_26.png', cv2.IMREAD_GRAYSCALE)
+    contours = to_contours(img)
+    contours_to_svg(contours, "top150.svg")
 
 
 
+
+#### zum Bild/Contours darstellen 
+
+# canvas = np.ones_like(edges) * 255  # weißer Hintergrund
+# cv2.drawContours(canvas, top_contours, -1, (0,0,0), 1)  # schwarze Linien, dicke=1
+
+# cv2.imshow("Top 100 Contours", cv2.resize(canvas,None, fx=0.8, fy=0.8))
+
+
+
+
+# cv2.imshow("Original", cv2.resize(img,None, fx=0.4, fy=0.4))
+# cv2.imshow("Segmented", cv2.resize(segmented,None, fx=0.4, fy=0.4))
+# cv2.imshow("blurred", cv2.resize(blurred,None, fx=0.4, fy=0.4))
+# cv2.imshow("edges", cv2.resize(edges,None, fx=0.8, fy=0.8))
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+###code mit dilation
 
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 # gray = cv2.GaussianBlur(gray, (5, 5), 0) #canny sonst empfindlich gegen Bildrauschen und krassen Kontrast
